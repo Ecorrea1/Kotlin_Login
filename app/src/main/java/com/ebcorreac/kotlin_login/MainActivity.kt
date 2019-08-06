00a77ee7f01e95514e8cbba7f54271e6e0ee4b37
Emmanuel Correa <ebcorreac@gmail.com>
2019-08-05 23:43:22 -0400

    agregando strings para colocarle idiomas extra como Ingles

--- app/src/main/java/com/ebcorreac/kotlin_login/MainActivity.kt
+++ app/src/main/java/com/ebcorreac/kotlin_login/MainActivity.kt
@@ -11,15 +11,21 @@ import kotlinx.android.synthetic.main.activity_registrar.*
 
 class MainActivity : AppCompatActivity() {
 
+
     private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //creando una instancia de autentificacion de firebase
         auth = FirebaseAuth.getInstance()
 
-        tv_createcount.setOnClickListener{
+            //llamando a la funcoion Onclick ded el textview de Crear cuenta
+            // de el Login a Registrar
 
+        tv_createcount.setOnClickListener{
+            //direccionando a la actividad Registrar
             startActivity (Intent(this,Registrar::class.java))
             finish()
 
